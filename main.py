d3f0b1936b8db99a124d4648d63f0d194fdd91b1
rishavgo <104450898+rishavgo@users.noreply.github.com>
2024-04-29 17:20:18 +0530

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-def pattern(n):
-      k = 2 * n - 2
-      for i in range(0,n):
-           for j in range(0,k):
-               print(end=" ")
-           k = k - 1
-           for j in range(0, i+1):
-                print("*", end=" ")
-           print("&#92r")
- 
-pattern(5)
+rows = int(input("Enter the number of rows:"))  
+k = 2 * rows - 2 
+for i in range(0, rows):  
+    for j in range(0, k):  
+        print(end=" ")  
+    k = k - 2   
+    for j in range(0, i + 1):  
+        print("* ", end="") 
+    print("") 
